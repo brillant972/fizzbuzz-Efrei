944e8d45f4422d15655c4f5da1c8cad7ec406889
brillant972 <br.bontrain@orange.fr>
2025-02-06 10:45:12 +0100

    refactor(main) : transformation en fonction

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def fizzbuzz(n):
+    result = ""
+    if n % 3 == 0:
+        result += "Fizz"
+    if n % 5 == 0:
+        result += "Buzz"
+    return result or str(n)
 
+def main():
+    for n in range(1, 101):
+        print(fizzbuzz(n))
+
+if __name__ == '__main__':
+    main()
